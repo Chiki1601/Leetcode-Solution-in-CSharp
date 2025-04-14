085214cd94a5980353dc5abb6715544f12ed3622
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-14 10:40:10 +0530

    Create 1534.cs

---
+++ 1501-2000/1534.cs
@@ -0,0 +1,5 @@
+public int CountGoodTriplets(int[] arr, int a, int b, int c) =>
+    Enumerable.Range(0, arr.Length - 2).Sum(i =>
+    Enumerable.Range(i + 1, arr.Length - 2 - i).Sum(j =>
+    Enumerable.Range(j + 1, arr.Length - 1 - j).Count(k =>
+        Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[i] - arr[k]) <= c)));
